29844f1acf9970ad89163ee241923a60edab0c01
Sarah Lean <13692824+weeyin83@users.noreply.github.com>
2025-08-07 13:11:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,5 +13,5 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
-/ Fake AWS secret access key for testing GitHub secret scanning
+// Fake AWS secret access key for testing GitHub secret scanning
 const awsSecretKey = "AKIA1234567890EXAMPLEFAKEKEY";
